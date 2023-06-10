e428af33dbbc35f36d560c6626e955d0ef1c9baa
Iago Valverde <iagopachiani96@gmail.com>
2023-06-10 15:48:16 -0300

    update main.js

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
 const form = document.getElementById('form-numeros');
+const numeroA = document.getElementById('campo-a');
+const numeroB = document.getElementById('campo-b');
 let formValido = false;
 
+const mensagemSucesso = "Correto: Campo B  maior que Campo A";
+const containerMensagemSucesso = document.getElementById('msg-sucesso');
+containerMensagemSucesso.innerHTML = mensagemSucesso;
+
+const mensagemErro = "[ERRO] Campo B precisa ser maior que Campo A";
+const containerMensagemErro = document.getElementById('msg-erro');
+containerMensagemErro.innerHTML = mensagemErro;
+
 function analisarMaiorNumero(primeiro, segundo) {
-    return segundo > primeiro;
+    return parseInt(segundo) > parseInt(primeiro);
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const numeroA = document.getElementById('campo-a').value;
-    const numeroB = document.getElementById('campo-b').value;
-    const mensagemSucesso = "Correto: Número B maior que Número A";
-    const mensagemErro = "[ERRO] Número A maior que Número B"
-
-    formValido = analisarMaiorNumero(numeroA, numeroB);
+    formValido = analisarMaiorNumero(numeroA.value, numeroB.value);
     if(formValido) {
-        const containerMensagemSucesso = document.getElementById('msg-sucesso');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
         containerMensagemSucesso.style.display = 'block';
+
+        numeroA.value = '';
+        numeroB.value = '';
     } else {
-        const containerMensagemErro = document.getElementById('msg-erro');
-        containerMensagemErro.innerHTML = mensagemErro;
         containerMensagemErro.style.display = 'block'
     }
-
 })
 
+numeroB.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    formValido = analisarMaiorNumero(numeroA.value, numeroB.value);
+
+    if(!formValido) {
+        numeroB.classList.add('erro');
+        containerMensagemErro.style.display = 'block';
+        containerMensagemSucesso.style.display = 'none';
+    } else {
+        numeroB.classList.remove('erro');
+        containerMensagemErro.style.display = 'none';
+    }
+})
